97a1afc919345acbe1cc264bc719ec32ddffdead
A.Lukjanenkovs <a.lukjanenkovs@student.rug.nl>
2016-07-16 15:08:11 +0300

    graphs_caches small refactoring

--- zeeguu/model/graphs_caches.py
+++ zeeguu/model/graphs_caches.py
@@ -19,14 +19,11 @@ class GraphsCaches(db.Model):
     piechart_cache_expire = db.Column(db.DateTime)
 
     def __init__(self, activity_graph_cache, line_graph_cache, piechart_cache, user, activity_graph_cache_expire, line_graph_cache_expire, piechart_cache_expire):
-        self.activity_graph_cache = activity_graph_cache
-        self.line_graph_cache = line_graph_cache
-        self.piechart_cache = piechart_cache
         self.user = user
-
-        self.activity_graph_cache_expire = activity_graph_cache_expire
-        self.line_graph_cache_expire = line_graph_cache_expire
-        self.piechart_cache_expire = piechart_cache_expire
+        
+        self.set_activity_graph_cache(activity_graph_cache, activity_graph_cache_expire)
+        self.set_line_graph_cache(line_graph_cache, line_graph_cache_expire)
+        self.set_piechart_cache(piechart_cache, piechart_cache_expire)
 
     def set_activity_graph_cache (self, activity_graph_cache, activity_graph_cache_expire):
         self.activity_graph_cache = activity_graph_cache
